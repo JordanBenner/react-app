74084478ca3087e86629b591421522e53b080339
Jordan Benner <jordanbenner@outlook.com>
2017-06-27 14:41:15 -0500

    Adding a click event

--- src/hello.jsx
+++ src/hello.jsx
@@ -1,9 +1,82 @@
-import React, {component} from "react";
+import React, {
+  Component
+} from 'react';
 
 class HelloMessage extends Component {
-render(){
-return <div>hello {this.props.name}</div>;
-}
-}
+  constructor(props) {
+    super(props);
+    this.state = date: new Date()
+    people: [{
+        id: 1,
+        name: 'Paul'
+      },
+      {
+        id: 2,
+        name: 'Paulette'
+      }
+    ]
+  }
 
-export default HelloMessage
+  componentDidMount() {
+    var self = this;
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState {
+      date: new Date()
+
+    });
+  // events
+  handlClick(event, p) {
+    if (p.selected) {
+      p.selected = false;
+    } else {
+      p.selected = true;
+    }
+    this.setState({people: this.state.people});
+  }
+
+    render() {
+      return ( <
+        div >
+        <
+        h1 > Hello {
+          this.props.name
+        } < /h1> <
+        h2 > It is {
+          this.state.date.toLocaleTimeString()
+        }. < /h2> <
+        ul >
+        // events
+        {
+          this.state.people.map((p) = >
+            <
+            li key = {
+              p.id
+            } >
+            <
+            button onClick = {
+              (e) => this.handleClick(e, p)
+            } > {
+              p.name
+            } <
+            /button> <
+            /li>
+
+          )
+        } <
+        /ul> <
+        /div>
+      )
+    }
+  }
+
+  export default HelloMessage;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
-import HelloMessage from './Hello';
+import HelloMessage from './hello.jsx';
 
 ReactDOM.render(<HelloMessage name="Jordan"/>, document.getElementById('root'));
 registerServiceWorker();
